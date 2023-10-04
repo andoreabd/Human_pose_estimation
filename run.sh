111acf0cf0e67ac3c6803aaed3597968a90cee94
Andrea <andrebd98@estudiantec.cr>
2023-10-03 19:50:31 -0600

    Running script

---
+++ run.sh
@@ -0,0 +1,5 @@
+python3 human_pose_estimation_demo.py \
+  -d CPU \
+  -i /home/root/Human_pose_estimation/human_pose_estimation_demo/python/face-demographics-walking.mp4 \
+  -m /home/root/Human_pose_estimation/human_pose_estimation_demo/python/human-pose-estimation-0005/FP16/human-pose-estimation-0005.xml \
+  -at ae
